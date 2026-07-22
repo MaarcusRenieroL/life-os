98519800f37df5d0d06d5f8711b7d143f78a0b17
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2026-07-22 14:32:04 +0530

    chore(batches): wire security config and fix kafka/flyway config

--- services/batches/src/main/java/com/lifeos/batches/BatchesApplication.java
+++ services/batches/src/main/java/com/lifeos/batches/BatchesApplication.java
@@ -2,7 +2,9 @@ package com.lifeos.batches;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.lifeos.batches", "com.lifeos.common"})
 @SpringBootApplication
 public class BatchesApplication {
 
